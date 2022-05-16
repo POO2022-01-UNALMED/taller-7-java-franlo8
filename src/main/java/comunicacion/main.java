9c2a5dd0383732c8816380e16fa1e8e35e4f0a6b
usuario <g>
2022-05-16 16:42:29 -0500

    yyyy

---
+++ src/main/java/comunicacion/main.java
@@ -0,0 +1,10 @@
+package comunicacion;
+
+public class main {
+    public static void main(String[] args) {
+        Libro libro = new Libro("Creacion", "El resplandor", "Stephen King", 599, "N/A", "Doubleday", "Primera", "Escrito largo");
+        System.out.println(libro.toString());
+    }
+
+
+}
